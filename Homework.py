41a81dc7b5dec314d73046718c04081b2b164a6c
saad09092012 <msaadatif@gmail.com>
2025-07-19 13:49:28 +0400

    Homework

---
+++ Homework.py
@@ -0,0 +1,17 @@
+# convert a string into Upper case
+x = "coNgrAtuLaTioNS"
+print(str.lower(x))
+
+print(str.upper(x))
+
+# Take two strings asinput from user
+print("Enter the First String: ")
+strOne = input()
+
+print("Enter the Second String: ")
+strTwo = input()
+
+# Concatenate the two strings
+# Using Addition Operater
+strThree = strOne + " "+ strTwo
+print("\nConcatenated String: ", strThree)
